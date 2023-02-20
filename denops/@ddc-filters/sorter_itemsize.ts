e9a52fc163acc160b9a9ef02c946eba9e478a377
ttak0422 <ttak0422@gmail.com>
2023-02-21 00:43:33 +0900

    Add: sameWordOnly flag

--- denops/@ddc-filters/sorter_itemsize.ts
+++ denops/@ddc-filters/sorter_itemsize.ts
@@ -1,18 +1,35 @@
 import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.2.0/types.ts";
 import { FilterArguments } from "https://deno.land/x/ddc_vim@v3.2.0/base/filter.ts";
 
-type Params = Record<string, number>;
+type Params = {
+  sameWordOnly: boolean;
+};
 
 export class Filter extends BaseFilter<Params> {
-  override filter(args: FilterArguments<Params>): Promise<Item<unknown>[]> {
-    return Promise.resolve(args.items.sort((a, b) => {
+  override filter(
+    { items, filterParams }: FilterArguments<Params>,
+  ): Promise<Item<unknown>[]> {
+    if (filterParams.sameWordOnly) {
+      return Promise.resolve(items.sort((a, b) => {
+        if (a.word === b.word && a.abbr && b.abbr) {
+          return a.abbr.length - b.abbr.length;
+        } else {
+          return 0;
+        }
+      }));
+    }
+
+    return Promise.resolve(items.sort((a, b) => {
       if (a.abbr && b.abbr) {
         return a.abbr.length - b.abbr.length;
       }
       return a.word.length - b.word.length;
     }));
   }
+
   override params(): Params {
-    return {};
+    return {
+      sameWordOnly: false,
+    };
   }
 }
